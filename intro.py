3aa4b7d37d5cacf33b84e8a910e696ad8d3527df
Vignesh Saikrishna Mallya <vignesh@appveen.com>
2023-02-27 09:20:13 +0530

    first commit

---
+++ intro.py
@@ -0,0 +1,15 @@
+item1 = "Phone"
+item1_price = 100
+item1_quantity = 100
+item1_total = item1_price * item1_quantity
+
+
+print(type(item1))
+print(type(item1_price))
+print(type(item1_quantity))
+print(type(item1_total))
+
+# item1 has been instantiated from class 'str'. other variables has been instantiated from the class 'int'
+# all variables are objects of class which is data type
+
+
